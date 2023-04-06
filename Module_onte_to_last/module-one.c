27256b3922f2549b8915198a349acd2cd5ebe0d2
Ayshi-Moni-0803 <ayshimoni0803@gmail.com>
2023-04-06 15:30:20 +0600

    3var add

--- Module_onte_to_last/module-one.c
+++ Module_onte_to_last/module-one.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    printf("hlw dreamless queen\n I am Ayshi Moni\n \t I am a student.\n I love cycling & programming\t \n I like to traveling\nI want a huge a lot of money for bought my all dreams\n");
-    printf("\\\n\t");
-    printf("%%\n\t");
-    printf("I love Trek cycle,mackbook,iphone");
+    int rahim=10;
+    float karim=2.5;
+    char butol='x';
+    printf("%d %0.2f %c",rahim,karim,butol);
     return 0;
 
 }
+
+
+    // printf("hlw dreamless queen\n I am Ayshi Moni\n \t I am a student.\n I love cycling & programming\t \n I like to traveling\nI want a huge a lot of money for bought my all dreams\n");
+    // printf("\\\n\t");
+    // printf("%%\n\t");
+    // printf("I love Trek cycle,mackbook,iphone");
